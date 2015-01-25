a961060ad46f1ef6862055e5f48a4692258094c9
Sebastian Rösch <msn@sebastianroesch.de>
2015-01-25 03:06:24 +0100

    Init

---
+++ WorldWeatherOnline.Client/WorldWeatherOnlineClient.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WorldWeatherOnline.Client.Models;
+
+namespace WorldWeatherOnline.Client
+{
+    /// <summary>
+    /// The client object to access the WorldWeatherOnline API
+    /// </summary>
+    public class WorldWeatherOnlineClient
+    {
+        private const string GeolookupAndCurrentConditionsUri = "http://api.worldweatheronline.com/free/v2/weather.ashx?key={0}&q={1},{2}&format=json&num_of_days=1";
+        private const string GeolookupCurrentConditionsAndForecastUri = "http://api.wunderground.com/api/{0}/geolookup/conditions/forecast/q/{1},{2}.json";
+        private const string GeolookupHourlyForecastUri = "http://api.wunderground.com/api/27d9503963b27155/geolookup/hourly/q/{1},{2}.json";
+
+        /// <summary>
+        /// Gets the current conditions for the specified coordinates
+        /// </summary>
+        /// <param name="lat">The latitude</param>
+        /// <param name="lng">The longitude</param>
+        /// <returns>The response object</returns>
+        public static async Task<WeatherResponse> GetConditionsForLocationAsync(double lat, double lng)
+        {
+            string uri = string.Format(GeolookupAndCurrentConditionsUri, Config.ApiKey, lat, lng);
+
+            using (var client = new HttpClient())
+            {
+                HttpResponseMessage response =  await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = response.Content.ReadAsStringAsync().Result;
+                    WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(content);
+
+                    if (weatherResponse.results != null && weatherResponse.results.error != null)
+                        throw new WeatherServiceException(weatherResponse.results.error.message);
+
+                    return weatherResponse;
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// The configuration for the WorldWeatherOnline Client
+        /// </summary>
+        public static class Config
+        {
+            /// <summary>
+            /// The API Key for the WorldWeatherOnline API. Get yours at http://www.worldweatheronline.com
+            /// </summary>
+            public static string ApiKey { get; set; }
+        }
+    }
+
+    /// <summary>
+    /// An exception thrown by the weather service
+    /// </summary>
+    public class WeatherServiceException : Exception
+    {
+        /// <summary>
+        /// Creates a new exception with the specified message
+        /// </summary>
+        /// <param name="message">The message</param>
+        public WeatherServiceException(string message) : base(message) { }
+    }
+}
---
+++ WorldWeatherOnline.Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldWeatherOnline.Client;
+using WorldWeatherOnline.Client.Models;
+
+namespace WorldWeatherOnline.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Configure the client by setting the API Key. Get yours at http://www.worldweatheronline.com
+            WorldWeatherOnline.Client.WorldWeatherOnlineClient.Config.ApiKey = ConfigurationSettings.AppSettings["ApiKey"];
+
+            //Get the current weather conditions for the specified location
+            WeatherResponse current = WorldWeatherOnlineClient.GetConditionsForLocationAsync(53.579724, 10.071984).Result;
+            Debug.WriteLine(current.data.current_condition.First().weatherDesc.First().value);
+
+            //Get the weather forecast for the specified location
+            //WeatherResponse forecast = WWorldWeatherOnlineClient.GetConditionsAndForecastForLocationAsync(51.4800, 0.0).Result;
+            //Debug.WriteLine(forecast.forecast.txt_forecast.forecastday[0].fcttext);
+        }
+    }
+}
